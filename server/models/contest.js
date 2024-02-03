1fb0921a9859dcef3ab6c289753a55743afbabff
unknown <hiepga123409@gmail.com>
2024-02-03 12:04:58 +0700

    first

---
+++ server/models/contest.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const contestSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    hostId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Contest", contestSchema);
